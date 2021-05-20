946cf5dfd956aaedd6ae38bc5f20bb73e3b5f027
Kirill Kornienko <k.kornienko@fast-report.com>
2021-05-20 15:43:46 +0300

    Updated targets file for using FrameworkReference

--- _Build_DataVisualization_Packages.bat
+++ _Build_DataVisualization_Packages.bat
@@ -9,5 +9,5 @@ rem that's all
 ECHO NOW TRY TO BUILD FR.DataVisualization
 
 pushd .\build
-   Powershell -ExecutionPolicy ByPass -File ".\build.ps1" --target=DataVisualization --solution-filename=DataViz.sln --config=Release  --vers=2021.2.0
+   Powershell -ExecutionPolicy ByPass -File ".\build.ps1" --target=DataVisualization --solution-filename=DataViz.sln --config=Release  --vers=2021.2.3
 popd
