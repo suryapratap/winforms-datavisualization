51661486d770cd703d38ad5d5bb9f93b3a819623
Kirill Kornienko <k.kornienko@fast-report.com>
2023-05-25 15:37:41 +0400

    added Demo configuration for Skia

--- build_WPF.bat
+++ build_WPF.bat
@@ -1,2 +1,2 @@
-dotnet pack src\FastReport.DataVisualization-Windows\Chart.WPF.csproj --configuration=Release /p:Version=2023.1.0
-dotnet pack src\FastReport.DataVisualization-Windows\Chart.WPF.csproj --configuration=Demo /p:Version=2023.1.0
+dotnet pack src\FastReport.DataVisualization-Windows\Chart.WPF.csproj --configuration=Release /p:Version=2023.2.0
+dotnet pack src\FastReport.DataVisualization-Windows\Chart.WPF.csproj --configuration=Demo /p:Version=2023.2.0
